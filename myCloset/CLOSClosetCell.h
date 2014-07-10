5673daa2bbbc70c66a95a5def12c447c07c8f4ed
Samantha <srwiener@gmail.com>
2014-07-10 14:04:18 -0700

    created closet cell for collection view on profile

---
+++ myCloset/CLOSClosetCell.h
@@ -0,0 +1,13 @@
+//
+//  CLOSClosetCell.h
+//  myCloset
+//
+//  Created by Samantha Wiener on 7/10/14.
+//  Copyright (c) 2014 ___rpinsker___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLOSClosetCell : UICollectionViewCell
+
+@end
---
+++ myCloset/CLOSClosetCell.m
@@ -0,0 +1,31 @@
+//
+//  CLOSClosetCell.m
+//  myCloset
+//
+//  Created by Samantha Wiener on 7/10/14.
+//  Copyright (c) 2014 ___rpinsker___. All rights reserved.
+//
+
+#import "CLOSClosetCell.h"
+
+@implementation CLOSClosetCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        NSLog(@"cell created");
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    NSLog(@"in drawRect");
+}
+
+
+@end
