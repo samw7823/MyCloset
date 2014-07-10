ba276b48b65c435fb4974729464fc83773538060
Ruoxi Tan <rtan@andrew.cmu.edu>
2014-07-10 10:05:56 -0700

    initialization
    with Parse

---
+++ myCloset/CLOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLOSAppDelegate.h
+//  myCloset
+//
+//  Created by Ruoxi Tan on 7/10/14.
+//  Copyright (c) 2014 SAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myCloset/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myCloset
+//
+//  Created by Ruoxi Tan on 7/10/14.
+//  Copyright (c) 2014 SAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLOSAppDelegate class]));
+    }
+}
